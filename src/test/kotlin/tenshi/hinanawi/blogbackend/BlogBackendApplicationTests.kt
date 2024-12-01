fa1e2b744622908f2c349141352dca3d918214d8
loremmlel <loremmlel@163.com>
2024-12-01 22:42:51 +0800

    feat: 初始化 blog-backend 项目- 创建项目结构和基本配置- 添加 Spring Boot 和 Kotlin 依赖 - 设置 Maven 构建配置 - 创建主应用和测试类

---
+++ src/test/kotlin/tenshi/hinanawi/blogbackend/BlogBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package tenshi.hinanawi.blogbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BlogBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
